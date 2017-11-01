7f1ed00961e3d9a715e8d1cec755739b8ae76595
sharath <sharathc443@gmail.com>
2017-10-31 19:57:02 -0500

    Project Commit

---
+++ projects/project50_AccessModifiers/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+/// <summary>
+/// This assembly has a type and type members
+/// we are going to practice some code using access modifiers
+/// </summary>
+namespace project50_AccessModifiersONE
+{
+    /// <summary>
+    /// This is a type which allows only public and internal.
+    ///  The default access modifier for a type is internal
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// This is a type member which has default access modifier of Private
+        /// </summary>
+        public void printOnePublic()
+        {
+            Console.WriteLine("This is a print method PUBLIC in project50_AccessModifiers class program ");
+
+        }
+        private void printTwoPrivate()
+        {
+            Console.WriteLine("This is a print method PRIVATE in project50_AccessModifiers class program ");
+
+        }
+        protected void printThreeProtected()
+        {
+            Console.WriteLine("This is a print method PROTECTED in project50_AccessModifiers class program ");
+
+        }
+        internal void printFourInternal()
+        {
+            Console.WriteLine("This is a print method INTERNAL in project50_AccessModifiers class program ");
+
+        }
+        protected internal void printFiveProtectedInternal()
+        {
+            Console.WriteLine("This is a print method PROTECTED INTERNAL in project50_AccessModifiers class program ");
+
+        }
+        static void Main(string[] args)
+        {
+            Program pr = new Program();
+            pr.printOnePublic();
+            pr.printTwoPrivate();
+            pr.printThreeProtected();
+            pr.printFourInternal();
+            pr.printFiveProtectedInternal();
+        }
+    }
+}
---
+++ projects/project50_AccessModifiersTWO/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using project50_AccessModifiersONE;
+
+namespace project50_AccessModifiersTWO
+{
+
+    internal class Class1:Program// The class has to be public to access its object C1
+    {
+        public static void Main()
+        {
+            Program P = new Program();
+            P.printOnePublic();
+            //See here only public method can be accessed not private and protected
+            Class1 C1 = new Class1();
+            C1.printFiveProtectedInternal();
+            // Here we can only access this protected Internal method using the object of current class object 
+            //but not with the class object where the method is implemented
+        }
+    }
+}
